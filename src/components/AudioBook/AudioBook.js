23ba94d831686c3bd27a700f7b5c8e87319cd454
Satyendra Kumar <satyendra.kumar@target.com>
2020-12-01 14:52:45 +0530

    added the download button in audio book page

--- src/components/AudioBook/AudioBook.js
+++ src/components/AudioBook/AudioBook.js
@@ -5,6 +5,8 @@ import 'react-h5-audio-player/lib/styles.css';
 import AudioBookCover from "../../images/Alice_in_Wonderland.jpg";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
+import Typography from "@material-ui/core/Typography";
+import {ReactComponent as Download} from '../../images/download.svg'
 
 const useStyles = makeStyles((theme) => ({
     gridList: {
@@ -33,6 +35,17 @@ const useStyles = makeStyles((theme) => ({
         width: "100% !important",
         height: "fit-content !important",
         padding: "0px 15px !important",
+    },
+    download: {
+
+        height: "48px",
+        width: "28px",
+        fill: "#00000090",
+      
+        '&:active': {
+          fill: "black",
+          cursor: "pointer",
+        }
     }
   }));
 
@@ -45,8 +58,13 @@ const AudioBook = () => {
         "author": "Lewis Carroll",
         "publisher": "Macmillan Publishers",
         "genres": "Fairy tale",
+        "url": "https://etc.usf.edu/lit2go/audio/mp3/alices-adventures-in-wonderland-001-chapter-i-down-the-rabbit-hole.1.mp3",
     }
 
+    // const downloadFile = () => {
+    //       window.location.href = "http://samarthanamcloudservices-env.eba-qe6ed46a.ap-south-1.elasticbeanstalk.com/downloadAudio?file=file_example_MP3_700KB.mp3"
+    // }
+    
     return (
         <div>
             <GridList cellHeight={300} spacing={30} className={classes.gridList}>
@@ -66,27 +84,31 @@ const AudioBook = () => {
                     <AudioPlayer
                         // src="http://devsharedstellentcontentsivr.target.com/target-com/flashvoicefiles/english/test1.mp3"
                         // src="http://samarthanamcloudservices-env.eba-qe6ed46a.ap-south-1.elasticbeanstalk.com/downloadAudio?file=file_example_MP3_700KB.mp3"
-                        src="https://etc.usf.edu/lit2go/audio/mp3/alices-adventures-in-wonderland-001-chapter-i-down-the-rabbit-hole.1.mp3"
+                        src={bookDetails.url}
                         onPlay={e => console.log("onPlay")}
                         progressJumpStep="10000" // 10 seconds
                         autoPlayAfterSrcChange={false}
                         defaultCurrentTime="00:00"
                         defaultDuration="loading..."
-                        customAdditionalControls={[]}
+                        customAdditionalControls={[
+                            <a
+                                href="http://samarthanamcloudservices-env.eba-qe6ed46a.ap-south-1.elasticbeanstalk.com/downloadAudio?file=file_example_MP3_700KB.mp3"
+                                download={"aa"}
+                            >
+                            <Download /*onClick={() => downloadFile()}*/ className={classes.download} />
+                            </a>
+                        ]}
                     />
                 </GridListTile>
-                {/* <GridListTile
-                    key="BookDetails"
-                    // className={classes.audioPlayer}
-                > */}
                 <div className={classes.bookDetails}>
-                    <p><b>Alice's Adventures in Wonderland</b></p>
-                    <p><b>Originally published: </b>26 November 1865</p>
-                    <p><b>Author: </b>Lewis Carroll</p>
-                    <p><b>Publisher: </b>Macmillan Publishers</p>
-                    <p><b>Genres: </b>Fairy tale</p>
+                    <Typography variant="h6">
+                        {bookDetails.title}
+                    </Typography> 
+                    <p><b>Originally published: </b>{bookDetails.published_date}</p>
+                    <p><b>Author: </b>{bookDetails.author}</p>
+                    <p><b>Publisher: </b>{bookDetails.publisher}</p>
+                    <p><b>Genres: </b>{bookDetails.genres}</p>
                 </div>
-{/* </GridListTile> */}
             </GridList>
         </div>
     );
