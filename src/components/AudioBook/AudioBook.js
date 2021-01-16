c13210874621d1fd9b2cae66963db129ea3b9615
Satyendra Kumar <satyendra.kumar@target.com>
2021-01-16 21:41:08 +0530

    updated AudioBook page to use URL from backend services

--- src/components/AudioBook/AudioBook.js
+++ src/components/AudioBook/AudioBook.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import AudioBookCover from "../../images/Alice_in_Wonderland.jpg";
@@ -7,8 +7,9 @@ import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import Typography from "@material-ui/core/Typography";
 import {ReactComponent as Download} from '../../images/download.svg'
+import axios from "axios";
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
     gridList: {
       display: "block",
       padding: "1%",
@@ -50,68 +51,90 @@ const useStyles = makeStyles((theme) => ({
           cursor: "pointer",
         }
     }
-  }));
+})
 
-const AudioBook = () => {
-    const classes = useStyles();
+const apiUrl = "http://ec2-13-232-236-83.ap-south-1.compute.amazonaws.com:8080/download_url?file_name=";
 
-    const bookDetails = {
-        "title": "Alice's Adventures in Wonderland",
-        "published_date": "26 November 1865",
-        "author": "Lewis Carroll",
-        "publisher": "Macmillan Publishers",
-        "genres": "Fairy tale",
-        "url": "http://samarthanamcloudservices-env.eba-qe6ed46a.ap-south-1.elasticbeanstalk.com/downloadAudio?file=alices-adventures-in-wonderland-001-chapter-i-down-the-rabbit-hole.1.mp3",
-    }
-
-    // const downloadFile = () => {
-    //       window.location.href = bookDetails.url
-    // }
-    
-    return (
-        <div>
-            <GridList cellHeight={300} spacing={30} className={classes.gridList}>
-                <GridListTile
-                    key="BookCover"
-                    className={classes.coverPage}
-                >
-                    <img src={AudioBookCover}
-                        alt="Book Cover"
-                        className={classes.coverPageImage}
-                    />
-                </GridListTile>
-                <GridListTile
-                    key="AudioPlayer"
-                    className={classes.audioPlayer}
-                >
-                    <AudioPlayer
-                        src={bookDetails.url}
-                        onPlay={e => console.log("onPlay")}
-                        progressJumpStep="10000" // 10 seconds
-                        autoPlayAfterSrcChange={false}
-                        defaultCurrentTime="00:00"
-                        defaultDuration="loading..."
-                        customAdditionalControls={[
-                            <a href={bookDetails.url}
-                                // download="Alice's Adventures in Wonderland.mp3"
-                            >
-                            <Download className={classes.download} />
-                            </a>
-                        ]}
-                    />
-                </GridListTile>
-                <div className={classes.bookDetails}>
-                    <Typography variant="h6" className={classes.bookTitle}>
-                        {bookDetails.title}
-                    </Typography> 
-                    <p><b>Originally published: </b>{bookDetails.published_date}</p>
-                    <p><b>Author: </b>{bookDetails.author}</p>
-                    <p><b>Publisher: </b>{bookDetails.publisher}</p>
-                    <p><b>Genres: </b>{bookDetails.genres}</p>
-                </div>
-            </GridList>
-        </div>
-    );
+const bookDetails = {
+    "title": "Alice's Adventures in Wonderland",
+    "file_name": "alices-adventures-in-wonderland-001-chapter-i-down-the-rabbit-hole.1.mp3",
+    "published_date": "26 November 1865",
+    "author": "Lewis Carroll",
+    "publisher": "Macmillan Publishers",
+    "genres": "Fairy tale",
+    "download_url": ""
 };
 
-export default AudioBook;
+class AudioBook extends React.Component  {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            download_url: ""
+        }
+    }
+
+    componentDidMount() {
+        axios
+        .get(apiUrl + bookDetails.file_name)
+        .then((response) => {
+            // console.log(response)
+            this.setState({"download_url" : response.data})
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        const { classes } = this.props
+        console.log(this.state.download_url)
+
+        return (
+            <div>
+                <GridList cellHeight={300} spacing={30} className={classes.gridList}>
+                    <GridListTile
+                        key="BookCover"
+                        className={classes.coverPage}
+                    >
+                        <img src={AudioBookCover}
+                            alt="Book Cover"
+                            className={classes.coverPageImage}
+                        />
+                    </GridListTile>
+                    <GridListTile
+                        key="AudioPlayer"
+                        className={classes.audioPlayer}
+                    >
+                        <AudioPlayer
+                            src={this.state.download_url}
+                            onPlay={e => console.log("onPlay")}
+                            progressJumpStep="10000" // 10 seconds
+                            autoPlayAfterSrcChange={false}
+                            defaultCurrentTime="00:00"
+                            defaultDuration="loading..."
+                            customAdditionalControls={[
+                                <a href={this.state.download_url} download /*target="_blank" rel="noopener noreferrer"*/
+                                    // download="Alice's Adventures in Wonderland.mp3"
+                                >
+                                <Download /*onClick={() => downloadFile()}*/ className={classes.download} />
+                                </a>
+                            ]}
+                        />
+                    </GridListTile>
+                    <div className={classes.bookDetails}>
+                        <Typography variant="h6" className={classes.bookTitle}>
+                            {bookDetails.title}
+                        </Typography> 
+                        <p><b>Originally published: </b>{bookDetails.published_date}</p>
+                        <p><b>Author: </b>{bookDetails.author}</p>
+                        <p><b>Publisher: </b>{bookDetails.publisher}</p>
+                        <p><b>Genres: </b>{bookDetails.genres}</p>
+                    </div>
+                </GridList>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(AudioBook);
